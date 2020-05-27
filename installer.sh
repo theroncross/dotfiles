28cc199b0556cab3ebc0eb8b4aeaf75c1df91dd0
Theron Cross <theroncross@gmail.com>
2020-05-27 09:42:41 -0700

    Add installer.sh

---
+++ installer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# vim
+ln -si ${BASEDIR}/vimrc ~/.vimrc
+ln -si ${BASEDIR}/vim/ ~/.vim
+
+#nvim
+ln -si ${BASEDIR}/.config/nvim/init.vim ~/.config/nvim/init.vim
+
+# git
+ln -si ${BASEDIR}/gitconfig ~/.gitconfig

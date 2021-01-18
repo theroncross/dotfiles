c3d4a406ab038946a0dcc1737fd5e474a4b57390
Theron Cross <theroncross@gmail.com>
2021-01-18 15:20:14 -0800

    Cleanup git aliases

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -3,6 +3,8 @@ alias g='git'
 #compdef g=git
 alias gst='git status'
 #compdef _git gst=git-status
+alias gstc='gst | grep \'both modified:\' | grep -oP \'\S+$\' | xargs nvim'
+#compdef _git gst=git-status
 alias gd='git diff'
 #compdef _git gd=git-diff
 alias gdc='git diff --cached'
@@ -22,29 +24,10 @@ end
 #compdef _git gdv=git-diff
 alias gc='git commit -v'
 #compdef _git gc=git-commit
-alias gc!='git commit -v --amend'
-#compdef _git gc!=git-commit
-alias gca='git commit -v -a'
-#compdef _git gc=git-commit
-alias gca!='git commit -v -a --amend'
-#compdef _git gca!=git-commit
-alias gcmsg='git commit -m'
+alias gcm='git commit -m'
 #compdef _git gcmsg=git-commit
 alias gco='git checkout'
 #compdef _git gco=git-checkout
-alias gcm='git checkout master'
-alias gr='git remote'
-#compdef _git gr=git-remote
-alias grv='git remote -v'
-#compdef _git grv=git-remote
-alias grmv='git remote rename'
-#compdef _git grmv=git-remote
-alias grrm='git remote remove'
-#compdef _git grrm=git-remote
-alias grset='git remote set-url'
-#compdef _git grset=git-remote
-alias grup='git remote update'
-#compdef _git grset=git-remote
 alias grbi='git rebase -i'
 #compdef _git grbi=git-rebase
 alias grbc='git rebase --continue'
@@ -57,53 +40,17 @@ alias gba='git branch -a'
 #compdef _git gba=git-branch
 alias gcount='git shortlog -sn'
 #compdef gcount=git
-alias gcl='git config --list'
 alias gcp='git cherry-pick'
 #compdef _git gcp=git-cherry-pick
-alias glg='git log --stat --max-count=10'
-#compdef _git glg=git-log
-alias glgg='git log --graph --max-count=10'
-#compdef _git glgg=git-log
-alias glgga='git log --graph --decorate --all'
-#compdef _git glgga=git-log
 alias glo='git log --oneline'
 #compdef _git glo=git-log
-alias gss='git status -s'
-#compdef _git gss=git-status
 alias ga='git add'
 #compdef _git ga=git-add
-alias gm='git merge'
-#compdef _git gm=git-merge
-alias grh='git reset HEAD'
-alias grhh='git reset HEAD --hard'
 alias gclean='git reset --hard; and git clean -dfx'
 alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
 
 alias gf='git fetch --all'
 
-alias gpoat='git push origin --all; and git push origin --tags'
-alias gmt='git mergetool --no-prompt'
-#compdef _git gm=git-mergetool
-
-alias gg='git gui citool'
-alias gga='git gui citool --amend'
-alias gk='gitk --all --branches'
-
-alias gsts='git stash show --text'
-alias gsta='git stash'
-alias gstp='git stash pop'
-alias gstd='git stash drop'
-
-# Will cd into the top of the current repository
-# or submodule.
-alias grt='cd (git rev-parse --show-toplevel; or echo ".")'
-
-# Git and svn mix
-alias git-svn-dcommit-push='git svn dcommit; and git push github master:svntrunk'
-#compdef git-svn-dcommit-push=git
-
-alias gsr='git svn rebase'
-alias gsd='git svn dcommit'
 #
 # Will return the current branch name
 # Usage example: git pull origin $(current_branch)
